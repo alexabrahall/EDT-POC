325033d1c85ff4e345d14c3b5631452bd64d89f1
Alex Abrahall <bongo120901@gmail.com>
2025-03-26 16:51:35 +0000

    timestuff

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
-import http.client
+from fast_flights import FlightData, Passengers, Result, get_flights
 
-conn = http.client.HTTPSConnection("iata-airports.p.rapidapi.com")
+result: Result = get_flights(
+    flight_data=[FlightData(date="2025-06-01", from_airport="BHX", to_airport="CDG")],
+    trip="one-way",
+    seat="economy",
+    passengers=Passengers(adults=2, children=0, infants_in_seat=0, infants_on_lap=0),
+    fetch_mode="common",
+)
 
-headers = {
-    'x-rapidapi-key': "81ed9cb398msh399a5fccc6b1cebp1320dejsn66e2049aceff",
-    'x-rapidapi-host': "iata-airports.p.rapidapi.com"
-}
+print(result)
 
-conn.request("GET", "/airports/BHX/", headers=headers)
-
-res = conn.getresponse()
-data = res.read()
-
-print(data.decode("utf-8"))
+# The price is currently... low/typical/high
+print("The price is currently", result.current_price)
